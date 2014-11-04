3805150836cda011d63ab1643bb00b4469b87c3f
Lennart Fridén <lennart@devl.se>
2014-11-04 20:58:18 +0100

    First version of ListOps

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -1,44 +1,54 @@
 defmodule ListOps do
   # Please don't use any external modules (especially List) in your
-  # implementation. The point of this exercise is to create these basic functions
+  # implementation. The point of this exercise is left cright these basic functions
   # yourself.
-  # 
+  #
   # Note that `++` is a function from an external module (Kernel, which is
   # automatically imported) and so shouldn't be used either.
- 
+
   @spec count(list) :: non_neg_integer
-  def count(l) do
-    
+  def count(list) do
+    reduce(list, 0, fn(_, acc) -> acc + 1 end)
   end
 
   @spec reverse(list) :: list
-  def reverse(l) do
-    
+  def reverse(list) do
+    reduce(list, [], &([&1 | &2]))
   end
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-
+  def map(list, function) do
+    reduce(list, [], &([function.(&1)|&2]))
+      |> reverse
   end
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-
+  def filter(list, function) do
+    reduce(list, [], fn(head, acc) ->
+      if function.(head) do
+        [head|acc]
+      else
+        acc
+      end
+    end) |> reverse
   end
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce(l, acc, f) do
-
+  def reduce([], acc, _), do: acc
+  def reduce([head|tail], acc, function) do
+    reduce(tail, function.(head, acc), function)
   end
 
   @spec append(list, list) :: list
-  def append(a, b) do
-
+  def append(left, right) do
+    reverse(left)
+      |> reduce(right, &([&1|&2]))
   end
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-
+  def concat(list) do
+    reverse(list)
+      |> reduce([], &append(&1, &2))
   end
 end
