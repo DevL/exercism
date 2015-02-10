1c6cc85b5224172283ed61eda42f7bd3ca09838b
Lennart Fridén <lennart@devl.se>
2015-02-10 16:37:41 +0100

    Initial stdlib-heavy solution.

---
+++ elixir/grade-school/school.exs
@@ -0,0 +1,31 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(Dict.t, String.t, pos_integer) :: Dict.t
+  def add(db, name, grade) do
+    Dict.merge(db, Dict.put(%{}, grade, [name]), fn(_key, left, right) -> left ++ right end)
+  end
+
+  @doc """
+  Return the names of the students in a particular grade.
+  """
+  @spec grade(Dict.t, pos_integer) :: [String]
+  def grade(db, grade) do
+    Dict.get(db, grade, [])
+  end
+
+  @doc """
+  Sorts the school by grade and name.
+  """
+  @spec sort(Dict) :: Dict.t
+  def sort(db) do
+    Enum.into(db, %{}, fn({grade, names}) -> {grade, Enum.sort(names)} end)
+  end
+end
