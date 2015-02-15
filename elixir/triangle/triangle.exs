0831037840ef8d87bbbb178652945fa510312f43
Lennart and Kim <lennart@devl.se>
2015-02-15 18:52:34 +0100

    Elixir/triangle - third version using macros

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -1,41 +1,38 @@
 defmodule Triangle do
   @type kind :: :equilateral | :isosceles | :scalene
 
+  defmacro any_not_positive?(a, b, c) do
+    quote do
+      unquote(a) <= 0 or unquote(b) <= 0 or unquote(c) <= 0
+    end
+  end
+
+  defmacro violating_triangle_rule?(a, b, c) do
+    quote do
+      unquote(a) + unquote(b) <= unquote(c) or
+        unquote(b) + unquote(c) <= unquote(a) or
+        unquote(c) + unquote(a) <= unquote(b)
+    end
+  end
+
+  defmacro all_different?(a, b, c) do
+    quote do
+      unquote(a) != unquote(b) and
+        unquote(b) != unquote(c) and
+        unquote(c) != unquote(a)
+    end
+  end
+
   @doc """
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
   @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
-  def kind(a, b, c) do
-    cond do
-      any_negative?(a, b, c) -> not_positive_error
-      violating_triangle_rule?(a, b, c) -> violating_triangle_rule_error
-      all_equal?(a, b, c) -> { :ok, :equilateral }
-      all_different?(a, b, c) -> { :ok, :scalene }
-      true -> { :ok, :isosceles }
-    end
-  end
+  def kind(a, b, c) when any_not_positive?(a, b, c), do: not_positive_error
+  def kind(a, b, c) when violating_triangle_rule?(a, b, c), do: violating_triangle_rule_error
 
-  defp all_equal?(same, same, same), do: true
-  defp all_equal?(_, _, _), do: false
-
-  defp all_different?(a, b, c) do
-    Enum.uniq([a, b, c]) == [a, b, c]
-  end
-
-  defp any_negative?(a, b, c) do
-    Enum.any? [a, b, c], fn(x) -> x <= 0 end
-  end
-
-  def violating_triangle_rule?(a, b, c) do
-    {smallest_pair, max} = sort_and_split(a, b, c)
-    Enum.sum(smallest_pair) <= hd(max)
-  end
-
-  defp sort_and_split(a, b, c) do
-    [a, b, c]
-    |> Enum.sort
-    |> Enum.split 2
-  end
+  def kind(same, same, same), do: { :ok, :equilateral }
+  def kind(a, b, c) when all_different?(a, b, c), do: { :ok, :scalene }
+  def kind(_, _, _), do: { :ok, :isosceles }
 
   defp not_positive_error, do: { :error, "all side lengths must be positive" }
   defp violating_triangle_rule_error, do: { :error, "side lengths violate triangle inequality" }
