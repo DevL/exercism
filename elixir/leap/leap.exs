849ddff337012599fc602cca33c2de2e0b90f48e
Lennart Fridén <lennart@devl.se>
2015-02-15 19:06:42 +0100

    Elixir/leap - cleanup using a macro.

--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -1,4 +1,10 @@
 defmodule Year do
+  defmacro divisble_by?(year, number) do
+    quote do
+      rem(unquote(year), unquote(number)) == 0
+    end
+  end
+
   @doc """
   Returns whether 'year' is a leap year.
 
@@ -9,7 +15,7 @@ defmodule Year do
       except every year that is evenly divisible by 400.
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) when rem(year, 400) == 0, do: true
-  def leap_year?(year) when rem(year, 100) == 0, do: false
-  def leap_year?(year), do: rem(year, 4) == 0
+  def leap_year?(year) when year |> divisble_by?(400) , do: true
+  def leap_year?(year) when year |> divisble_by?(100), do: false
+  def leap_year?(year), do: year |> divisble_by?(4)
 end
