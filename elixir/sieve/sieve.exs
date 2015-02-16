4b651a7ea3e2801b08a399fb03b819e76a8d35e9
Lennart Fridén <lennart@devl.se>
2015-02-16 18:16:04 +0100

    Elixir/sieve - initial solution

---
+++ elixir/sieve/sieve.exs
@@ -0,0 +1,24 @@
+defmodule Sieve do
+  @doc """
+  Generates a list of primes up to a given limit.
+  """
+  @spec primes_to(non_neg_integer) :: [non_neg_integer]
+  def primes_to(limit) do
+    2..limit
+    |> Enum.to_list
+    |> collect_primes([])
+    |> Enum.reverse
+  end
+
+  defp collect_primes([], primes), do: primes
+  defp collect_primes([prime|candidates], primes) do
+    collect_primes(remove_candidates(prime, candidates), [prime|primes])
+  end
+
+  defp remove_candidates(prime, candidates) do
+    candidates
+    |> Enum.reject &divisable_by(&1, prime)
+  end
+
+  defp divisable_by(candidate, prime), do: rem(candidate, prime) == 0
+ end
