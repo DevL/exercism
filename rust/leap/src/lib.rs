d7fa65475e3b79d59cc08db59fbc202115a9be7f
Lennart Fridén <lennart@devl.se>
2020-02-29 22:54:47 +0100

    Fix typo

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,12 +1,18 @@
 pub fn is_leap_year(year: u64) -> bool {
   match year {
-    year if divisible_by(&year, 400) => true,
-    year if divisible_by(&year, 100) => false,
-    year if divisible_by(&year, 4) => true,
+    year if year.divisible_by(400) => true,
+    year if year.divisible_by(100) => false,
+    year if year.divisible_by(4) => true,
     _ => false
   }
 }
 
-fn divisible_by(year: &u64, divisor: u64) -> bool {
-  year % divisor == 0
+trait Divisible<T> {
+  fn divisible_by(self, divisor: T) -> bool;
+}
+
+impl Divisible<u64> for u64 {
+  fn divisible_by(self: u64, divisor: u64) -> bool {
+    self % divisor == 0
+  }
 }
